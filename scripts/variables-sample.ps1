5dcb4c173dcaa0beb212380b6ad310dacdf88e95
Julián Hidalgo <julian.hidalgo@gmail.com>
2024-08-23 01:18:04 +0200

    .

--- scripts/variables-sample.ps1
+++ scripts/variables-sample.ps1
@@ -2,12 +2,40 @@ $resourceGroupName = "Kanva-Pilot"
 $location = "norwaywest"
 $subscriptionId = "Your-Subscription-ID"
 
-# Storage account
-$storageAccountName = "kanvapilotstorage"
-$sku = "Standard_LRS"
-$kind = "StorageV2"
-$accessTier = "Hot"
+$location = "norwayeast"
+$resourceGroupName = "Kanva-Pilot"
+$subscriptionId = ""
 
+# Storage account
+$accessTier = "Hot"
+$kind = "StorageV2"
+$sku = "Standard_LRS"
+$storageAccountName = "kanvapilotstorage"
+
+# Key Vault
+$keyVaultName = "kanvapilotkv"
+$registryPasswordSecretName = "kanvaRegistryPassword"
+$registryPassword = "erVyzwGay8UkHzVEsNqKzi/2ZO4lj2HIBx+rwLMjm8+ACRAwmfvb"
+
+# Database
+$autoPauseDelay = 60
+$backupStorageRedundancy = "Local"
+$capacity = 2
+$computeModel = "Serverless"
+$databaseName = "kanva-pilot-projects"
+$edition = "GeneralPurpose"
+$family = "Gen5"
+$minCapacity = 0.5
+$serverName = "kanva-pilots-db"  # This matches the default value in the template
+
+# Container Apps
+$certificateId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.App/managedEnvironments/$environmentName/managedCertificates/kanva.human-driven.ai-managede-240413074901"
+$customDomainName = "kanva.human-driven.ai"
+$environmentName = "managedEnvironment-KanvaPilot"
+$fqdn = "kanva.whiterock-7e873a3c.norwayeast.azurecontainerapps.io"
+$registryPasswordSecretRef = "reg-pswd-829f0db6-bbd7"
+$registryServer = "kanvaimages.azurecr.io"
+$registryUsername = "kanvaimages"
 # Valid locations
 <#
 australiacentral
