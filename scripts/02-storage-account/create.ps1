90f819192a00457a8e0ff8fc61664d5ae3fd2b69
Julián Hidalgo <julian.hidalgo@gmail.com>
2024-08-23 04:42:10 +0200

    Add file share.

--- scripts/02-storage-account/create.ps1
+++ scripts/02-storage-account/create.ps1
@@ -11,3 +11,17 @@ az storage account create `
     --allow-cross-tenant-replication false `
     --min-tls-version TLS1_2 `
     --public-network-access Enabled
+
+# Get the storage account key
+$storageAccountKey = (az storage account keys list --resource-group $resourceGroupName --account-name $storageAccountName --query '[0].value' -o tsv)
+
+# Create file share
+az storage share create `
+    --name $fileShareName `
+    --account-name $storageAccountName `
+    --account-key $storageAccountKey `
+    --quota $fileShareQuota
+
+# Display the storage account key
+Write-Host "Storage Account Key: $storageAccountKey"
+Write-Host "Please update variables.ps1 with the storage account key" -ForegroundColor DarkGreen -BackgroundColor White
