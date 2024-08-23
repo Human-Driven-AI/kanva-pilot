8cbaeaaf3b56199b9d1c261fd399e6b0e491811f
Nick Rivas <1537588+nickrivas@users.noreply.github.com>
2024-08-22 21:01:22 -0400

    SQL database creation script

--- scripts/03-projects-database/create.ps1
+++ scripts/03-projects-database/create.ps1
@@ -1,1 +1,23 @@
 . "$PSScriptRoot\..\variables.ps1"
+az sql server create `
+    --name $serverName `
+    --resource-group $resourceGroupName `
+    --location $location `
+    --admin-user $adminUser `
+    --admin-password $adminPassword
+
+az sql db create `
+    --resource-group $resourceGroupName `
+    --server $serverName `
+    --name $databaseName `
+    --edition GeneralPurpose `
+    --family Gen5 `
+    --capacity $capacity `
+    --zone-redundant false `
+    --collation "SQL_Latin1_General_CP1_CI_AS" `
+    --max-size 2GB `
+    --read-replicas 0 `
+    --auto-pause-delay -1 `
+    --min-capacity 0.5 `
+    --backup-storage-redundancy Local `
+    --compute-model $computeModel
