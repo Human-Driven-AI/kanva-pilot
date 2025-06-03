65188495df4f1090bb0b637e25e6d3737b92b574
Julián Hidalgo <julian.hidalgo@gmail.com>
2025-06-03 12:51:03 +0200

    Added bat file to update Kanva on Windows.

---
+++ windows/update-kanva.bat
@@ -0,0 +1,6 @@
+docker-compose down
+docker pull kanvaimages.azurecr.io/efbundle:latest
+docker pull kanvaimages.azurecr.io/hub:latest
+docker pull kanvaimages.azurecr.io/delphi:latest
+docker pull kanvaimages.azurecr.io/pythoness:latest
+docker-compose up -d
